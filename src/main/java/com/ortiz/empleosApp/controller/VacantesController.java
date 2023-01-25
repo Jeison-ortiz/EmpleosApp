9fe7f80057d18a80bcdf81c4ecb2b6c0d78b6ff2
Jeison Ortiz <ortizjeison4@gmail.com>
2023-01-25 17:01:49 -0500

    controller | templates (feat): - Se agregan plantillas para el formulario de la vacante y para listar las vacantes. - Se relaciona las plantillas del formulario, con la de listado.

--- src/main/java/com/ortiz/empleosApp/controller/VacantesController.java
+++ src/main/java/com/ortiz/empleosApp/controller/VacantesController.java
@@ -1,9 +1,15 @@
 package com.ortiz.empleosApp.controller;
 
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,6 +24,17 @@ public class VacantesController {
 
 	@Autowired
 	IVacanteService vacanteService;
+	
+	@GetMapping("/crear")
+	public String crear() {
+		return "vacantes/formVacantes";
+	}
+	
+	@GetMapping("/save")
+	public String guardar() {
+		return "vacantes/listVacantes";
+	}
+	
 	
 	@GetMapping("/view/{id}")
 	public String verDetalle(@PathVariable("id") int idVacante, Model model) {
@@ -36,5 +53,10 @@ public class VacantesController {
 		return "vacantes/mensaje";
 	}
 	
-	
+	@InitBinder
+	public void initBinder(WebDataBinder webDataBinder) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+		webDataBinder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, false));
+	}
+		
 }
