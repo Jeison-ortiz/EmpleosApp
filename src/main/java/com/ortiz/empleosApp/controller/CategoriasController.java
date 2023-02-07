28da173f96ca76320f79aa8748a148fdc7e6298f
Jeison Ortiz <ortizjeison4@gmail.com>
2023-02-07 18:53:39 -0500

    feat (controller): - Se agrega endpoint para listar las categorias

--- src/main/java/com/ortiz/empleosApp/controller/CategoriasController.java
+++ src/main/java/com/ortiz/empleosApp/controller/CategoriasController.java
@@ -1,6 +1,7 @@
 package com.ortiz.empleosApp.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,12 @@ public class CategoriasController {
 
 	@GetMapping("/index")
 	public String mostrarIndex() {
+		return "categorias/listCategorias";
+	}
+	
+	@GetMapping("/listCategorias")
+	public String listarVacantes(Model model) {
+		System.out.println("Se llama a listar vacantes");
 		return "categorias/listCategorias";
 	}
 	
