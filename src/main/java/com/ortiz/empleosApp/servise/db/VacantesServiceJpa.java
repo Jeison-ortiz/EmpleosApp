d678af327e078ca741fecc11a54a5fd162907103
Jeison Ortiz <ortizjeison4@gmail.com>
2023-02-22 23:38:12 -0500

    feat(Service): - Se implementa la interfaz VacantesServiceJpa

--- src/main/java/com/ortiz/empleosApp/servise/db/VacantesServiceJpa.java
+++ src/main/java/com/ortiz/empleosApp/servise/db/VacantesServiceJpa.java
@@ -1,27 +1,36 @@
 package com.ortiz.empleosApp.servise.db;
 
 import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.ortiz.empleosApp.model.Vacante;
+import com.ortiz.empleosApp.repository.VacantesRepository;
 import com.ortiz.empleosApp.service.IVacanteService;
 
 public class VacantesServiceJpa implements IVacanteService {
 
+	@Autowired
+	private VacantesRepository vacantesRepo;
+	
 	@Override
 	public List<Vacante> buscarTodas() {
-		// TODO Auto-generated method stub
-		return null;
+		return vacantesRepo.findAll();
 	}
 
 	@Override
 	public Vacante buscarPorId(Integer idVacante) {
-		// TODO Auto-generated method stub
+		Optional<Vacante> optional = vacantesRepo.findById(idVacante);
+		if(optional.isPresent()) {
+			optional.get();
+		}
 		return null;
 	}
 
 	@Override
 	public void guardar(Vacante vacante) {
-		// TODO Auto-generated method stub
+		vacantesRepo.save(vacante);
 		
 	}
 
